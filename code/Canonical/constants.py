1cb5cc0ee781be71cd7d4b72ecbe411b74ffaf51
cynthiaxu04 <89044958+cynthiaxu04@users.noreply.github.com>
2023-07-09 10:57:59 -0700

    Add files via upload

--- code/Canonical/constants.py
+++ code/Canonical/constants.py
@@ -7,10 +7,10 @@ SL=5000
 # Sequence length of SpliceAIs (SL+CL will be the input length and
 # SL will be the output length)
 
-splice_table='canonical_dataset.txt'
-ref_genome='/genomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa'
+splice_table='./Canonical/data/canonical_dataset.txt'
+ref_genome='./data/hg19.fa'
 # Input details
 
-data_dir='./'
-sequence='canonical_sequence.txt'
+data_dir='./Canonical/data/'
+sequence='./Canonical/data/canonical_sequence.txt'
 # Output details
